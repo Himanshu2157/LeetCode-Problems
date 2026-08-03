71d4d50d8f11e1a1f3642adc4b70ad0b0e859d49
DSA <himanshubansal806@gmail.com>
2026-08-03 21:34:36 +0530

    Two Sum Problem 1.2

--- two sum.c
+++ two sum.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int front, rear;
-    returnSize = (int*) malloc(2*sizeof(int));
-    front=0; rear=numsSize-1;
-    int found = 0;
-    while(1){
-        if (nums[front]+nums[rear]>target)
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int front = 0;
+    int rear = numsSize - 1;
+
+    while (front < rear)
+    {
+        int sum = nums[front] + nums[rear];
+
+        if (sum > target)
             rear--;
-        else if (nums[front]+nums[rear]<target)
+        else if (sum < target)
             front++;
-        else if (nums[front]+nums[rear]==target){
-            returnSize[0]=front;
-            returnSize[1]=rear;
-            found = 1;
-            break;}
-        else{
-            break;}
+        else
+        {
+            int *ans = malloc(2 * sizeof(int));
+            ans[0] = front;
+            ans[1] = rear;
+            *returnSize = 2;
+            return ans;
+        }
     }
-    if(found){
-        return returnSize;
-    }
+
+    *returnSize = 0;
     return NULL;
 }
 
-int main(){
+int main()
+{
     int arr[] = {11,45,56,78,91};
-    int size = sizeof(arr)/sizeof(int);
-    int sum = 123;
-    int arr2[2];
-    int* array = twoSum(arr,size,sum,arr2);
-    printf("%d",*array);
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 123;
+
+    int returnSize;
+    int *array = twoSum(arr, size, target, &returnSize);
+
+    if (array != NULL)
+    {
+        printf("%d %d\n", array[0], array[1]);
+        free(array);
+    }
+    else
+    {
+        printf("No pair found.\n");
+    }
+
     return 0;
 }
