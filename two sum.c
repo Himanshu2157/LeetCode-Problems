5f06ec519546ee29d663bf4f614e39f84e0bea45
DSA <himanshubansal806@gmail.com>
2026-08-03 21:32:10 +0530

    Two Sum Problem 1.1

--- two sum.c
+++ two sum.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int front, rear;
+    returnSize = (int*) malloc(2*sizeof(int));
+    front=0; rear=numsSize-1;
+    int found = 0;
+    while(1){
+        if (nums[front]+nums[rear]>target)
+            rear--;
+        else if (nums[front]+nums[rear]<target)
+            front++;
+        else if (nums[front]+nums[rear]==target){
+            returnSize[0]=front;
+            returnSize[1]=rear;
+            found = 1;
+            break;}
+        else{
+            break;}
+    }
+    if(found){
+        return returnSize;
+    }
+    return NULL;
+}
 
 int main(){
     int arr[] = {11,45,56,78,91};
     int size = sizeof(arr)/sizeof(int);
-    int front, rear;
     int sum = 123;
-    front=0; rear=size-1;
-    while(1){
-        if (arr[front]+arr[rear]>sum)
-            rear--;
-        else if (arr[front]+arr[rear]<sum)
-            front++;
-        else if (arr[front]+arr[rear]==sum){
-            printf("Sum found at index: %d and %d\n", front, rear);
-            break;}
-        else{
-            printf("No index sum matches");
-            break;}
-    }
+    int arr2[2];
+    int* array = twoSum(arr,size,sum,arr2);
+    printf("%d",*array);
     return 0;
 }
