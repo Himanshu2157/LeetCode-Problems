bf69f77a8255dd8c266277877a96d6e3cb487101
DSA <himanshubansal806@gmail.com>
2026-08-03 22:32:25 +0530

    Two Sum Problem 1.3

--- two sum.c
+++ two sum.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void insertion_sort(int* arr, int size){
+    for(int i = 1; i<=size-1;i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]= key;}
+}
+int* copy(int* arr, int size){
+    int* arr_copy = (int*)malloc(sizeof(int)*size);
+    for(int i=0; i<size; i++){
+        arr_copy[i] = arr[i];
+    }
+    return arr_copy;
+}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
+    int* numsCopy = copy(nums, numsSize);
+    insertion_sort(nums,numsSize);
     int front = 0;
     int rear = numsSize - 1;
-
+    int *ans = malloc(2 * sizeof(int));
     while (front < rear)
     {
         int sum = nums[front] + nums[rear];
@@ -16,9 +36,14 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
             front++;
         else
         {
-            int *ans = malloc(2 * sizeof(int));
-            ans[0] = front;
-            ans[1] = rear;
+            for(int i=0; i<numsSize; i++){
+                if(numsCopy[i]==nums[front]){
+                    ans[0] = i;
+                }
+                if(numsCopy[i]==nums[rear]){
+                    ans[1] = i;
+                }
+            }
             *returnSize = 2;
             return ans;
         }
@@ -30,9 +55,9 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 
 int main()
 {
-    int arr[] = {11,45,56,78,91};
+    int arr[] = {3,2,4};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 123;
+    int target = 6;
 
     int returnSize;
     int *array = twoSum(arr, size, target, &returnSize);
