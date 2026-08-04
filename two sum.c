a1f084d175d20acd5a2d413549e9062a72f99fd2
DSA <himanshubansal806@gmail.com>
2026-08-05 00:14:55 +0530

    Two Sum Problem 1.5, Improved Code (takes less time and space)

--- two sum.c
+++ two sum.c
@@ -2,68 +2,91 @@
 #include <stdlib.h>
 #include <limits.h>
 
-void insertion_sort(int* arr, int size){
-for(int i = 1; i<=size-1;i++){
-int key = arr[i];
-int j = i-1;
-while(j>=0 && arr[j]>key){
-arr[j+1]=arr[j];
-j--;
+void insertion_sort(int* arr, int size)
+{
+    for (int i = 1; i <= size - 1; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
 }
-arr[j+1]= key;}
-}
-int* copy(int* arr, int size){
-int* arr_copy = (int*)malloc(sizeof(int)*size);
-for(int i=0; i<size; i++){
-arr_copy[i] = arr[i];
-}
-return arr_copy;
+
+int* copy(int* arr, int size)
+{
+    int* arr_copy = (int*)malloc(sizeof(int) * size);
+
+    for (int i = 0; i < size; i++)
+    {
+        arr_copy[i] = arr[i];
+    }
+
+    return arr_copy;
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
-int* numsCopy = copy(nums, numsSize);
-insertion_sort(nums,numsSize);
-int front = 0;
-int rear = numsSize - 1;
-int *ans = malloc(2 * sizeof(int));
-while (front < rear)
-{
-int sum = nums[front] + nums[rear];
+    int* numsCopy = copy(nums, numsSize);
 
-if (sum > target)  
-        rear--;  
-    else if (sum < target)  
-        front++;  
-    else  
-    {  
-        for(int i=0; i<numsSize; i++){  
-            if(numsCopy[i]==nums[front]){  
-                ans[0] = i;  
-                nums[front] = INT_MIN;  
-            }  
-            if(numsCopy[i]==nums[rear]){  
-                ans[1] = i;  
-            }  
-        }  
-        *returnSize = 2;  
-        return ans;  
-    }  
-}  
+    insertion_sort(nums, numsSize);
 
-*returnSize = 0;  
-return NULL;
+    int front = 0;
+    int rear = numsSize - 1;
+    int* ans = malloc(2 * sizeof(int));
 
+    while (front < rear)
+    {
+        int sum = nums[front] + nums[rear];
+
+        if (sum > target)
+        {
+            rear--;
+        }
+        else if (sum < target)
+        {
+            front++;
+        }
+        else
+        {
+            for (int i = 0; i < numsSize; i++)
+            {
+                if (numsCopy[i] == nums[front])
+                {
+                    ans[0] = i;
+                    nums[front] = INT_MIN;
+                }
+
+                if (numsCopy[i] == nums[rear])
+                {
+                    ans[1] = i;
+                }
+            }
+            free(numsCopy);
+            *returnSize = 2;
+            return ans;
+        }
+    }
+    
+    free(numsCopy);
+    *returnSize = 0;
+    return NULL;
 }
 
 int main()
 {
-    int arr[] = {3,3};
+    int arr[] = {3, 3};
     int size = sizeof(arr) / sizeof(arr[0]);
     int target = 6;
 
     int returnSize;
-    int *array = twoSum(arr, size, target, &returnSize);
+    int* array = twoSum(arr, size, target, &returnSize);
 
     if (array != NULL)
     {
