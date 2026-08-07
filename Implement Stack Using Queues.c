8e239c1f2bc6ce36f028a5454038517c8d953007
DSA <himanshubansal806@gmail.com>
2026-08-08 01:03:43 +0530

    Implement Stack Using Queues 1.1

--- Implement Stack Using Queues.c
+++ Implement Stack Using Queues.c
@@ -10,12 +10,20 @@ typedef struct {
     int* arr;
 } MyStack;
 
+void traversal(MyStack* stack){
+    int i = stack->front+1;
+    while(i<=stack->rear){
+        printf("%d\t",stack->arr[i]);
+        i++;
+    }
+    printf("\n");
+}
 
 MyStack* myStackCreate() {
     MyStack* obj = (MyStack*)malloc(sizeof(MyStack));
     obj->front=-1;
     obj->rear=-1;
-    obj->size=5;
+    obj->size=100;
     obj->arr = (int*)malloc(sizeof(int)*obj->size);
     return obj;
 }
@@ -42,27 +50,34 @@ void myStackPush(MyStack* obj, int x) {
     }
 }
 
-MyStack* myStackPop(MyStack* obj) {
+int myStackPop(MyStack* obj) {
     if (myStackEmpty(obj))
-    return obj;
+    return -1;
     
     MyStack* obj2 = myStackCreate();
     if(obj->front==-1)
         obj->front=0;
     while(obj->front<obj->rear){
         myStackPush(obj2,obj->arr[obj->front]);
-        printf("%d pushed into stack\n",obj->arr[obj->front]);
+        printf("%d pushed into stack 2\n",obj->arr[obj->front]);
         obj->front++;
     }
+    printf("\nobj->rear = %d\nobj->front = %d\n",obj->rear,obj->front);
+    printf("\nobj2->rear = %d\nobj2->front = %d\nObj2:\n",obj2->rear,obj2->front);
+    traversal(obj2);
     int val = obj->arr[obj->front];
-    printf("%d removed from stack\n", val);
-    free(obj);
-    return obj2;
+    while(obj2->front<=obj2->rear){
+        if(obj2->front>-1)
+        myStackPush(obj,obj2->arr[obj2->front]);
+        obj2->front++;
+    }
+    traversal(obj);
+    return val;
 }
 
 int myStackTop(MyStack* obj) {
     if(myStackEmpty(obj))
-        return -1;
+    return -1;
     return obj->arr[obj->rear];
 }
 
@@ -83,30 +98,18 @@ void myStackFree(MyStack* obj) {
  * bool param_4 = myStackEmpty(obj);
  
  * myStackFree(obj);
-*/
-void traversal(MyStack* stack){
-    int i = 0;
-    while(i<=stack->rear){
-        printf("%d\t",stack->arr[i]);
-        i++;
-    }
-    printf("\n");
-}
+ */
 int main(){
     MyStack* stack = myStackCreate();
-    printf("%d\n",myStackEmpty(stack));
     myStackPush(stack,13);
-    // stack = myStackPop(stack);
     myStackPush(stack,58);
     myStackPush(stack,71);
     myStackPush(stack,43);
-    printf("%d\n",myStackFull(stack));
-    myStackPush(stack,43);
-    printf("%d\n",myStackFull(stack));
-    myStackPush(stack,43);
-    printf("%d\n",myStackFull(stack));
     traversal(stack);
-    // stack = myStackPop(stack);
+    int removed = myStackPop(stack);
+    printf("%d removed from stack\n", removed);
+    myStackPush(stack,46);
+    myStackPush(stack,75);
     traversal(stack);
     return 0;
 }
